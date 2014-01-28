ed66f98b4e79a91b8a8a0bd19efa5706fa1d6b0b
Matthew C. Jones <mjones@isaudits.com>
2014-01-28 11:35:48 -0500

    Add verbosity option

--- ike-scan.py
+++ ike-scan.py
@@ -16,12 +16,14 @@ import re
 def main(argv):
     parser = argparse.ArgumentParser(description='Run ike-scan transforms against a target')
     parser.add_argument("target_ip", action="store", help="Target IP address")
+    parser.add_argument("-v","--verbose", action="store_true", help="verbose - show all ike-scan output")
     parser.add_argument("--showbackoff", action="store_true", help="Show backoff (for fingerprinting)")
     parser.add_argument("--aggressive", action="store_true", help="Use IKE aggressive mode instead of main mode")
     parser.add_argument("--pskcrack", action="store", help="Output aggressive mode PSK in pskcrack format with optional output file")
     parser.add_argument("--id", action="store", help="specify id to pass in aggressive mode handshake (Sonicwall default is 'GroupVPN'; Cisco default is peer IP)")
     parser.add_argument("--extended", action="store_true", help="extended scan of all possible transforms; default is to stop after first handshake")
     parser.add_argument("--allresponses", action="store_true", help="show all responses, even if only notify response")
+    
     
     args = parser.parse_args()  
     
@@ -68,7 +70,9 @@ def main(argv):
                     regex_cisco = 'Cisco'
                     regex_ciscodpd = 'Dead Peer'
                     
-                    if args.allresponses:
+                    if args.verbose:
+                        matchObj = True
+                    elif args.allresponses:
                         matchObj = re.search(regex_notify, out)
                     else:
                         matchObj = re.search(regex_handshake, out)          
