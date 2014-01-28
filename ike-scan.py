682e1c29a667b9e8bcd28baa65ad972aefba49c2
Matthew C. Jones <mjones@isaudits.com>
2014-01-28 12:28:40 -0500

    Add alltransforms option

--- ike-scan.py
+++ ike-scan.py
@@ -17,11 +17,13 @@ def main(argv):
     parser = argparse.ArgumentParser(description='Run ike-scan transforms against a target')
     parser.add_argument("target_ip", action="store", help="Target IP address")
     parser.add_argument("-v","--verbose", action="store_true", help="verbose - show all ike-scan output")
+    parser.add_argument("-a", "--alltransforms", action="store_true", help="show all responses, even if only notify response")
     parser.add_argument("--showbackoff", action="store_true", help="Show backoff (for fingerprinting)")
     parser.add_argument("--aggressive", action="store_true", help="Use IKE aggressive mode instead of main mode")
     parser.add_argument("--pskcrack", action="store", help="Output aggressive mode PSK in pskcrack format with optional output file")
     parser.add_argument("--id", action="store", help="specify id to pass in aggressive mode handshake (Sonicwall default is 'GroupVPN'; Cisco default is peer IP)")
-    parser.add_argument("--extended", action="store_true", help="extended scan of all possible transforms; default is to stop after first handshake")
+    parser.add_argument("--idtype", action="store", help="specify id type to pass in aggressive mode handshake (e.g. 1)")
+    parser.add_argument("--scanall", action="store_true", help="continue scanning through all transforms; default is to stop after first handshake recd")
     parser.add_argument("--allresponses", action="store_true", help="show all responses, even if only notify response")
     
     
@@ -33,14 +35,26 @@ def main(argv):
         print("Need root privileges to function properly; Re-run as sudo...")
         sys.exit()
     
-    # Encryption algorithms: DES, Triple-DES, AES/128, AES/192 and AES/256
-    list_enc=['1', '5', '7/128', '7/192', '7/256']
-    # Hash algorithms: MD5 and SHA1
-    list_hash=['1','2']
-    # Authentication methods: Pre-Shared Key, RSA Signatures, Hybrid Mode and XAUTH
-    list_auth=['1','3', '64221', '65001']
-    # Diffie-Hellman groups: 1, 2 and 5
-    list_dhgroup=['1','2', '5']
+    # Encryption algorithms: 
+    if args.alltransforms:
+        list_enc=['1','2', '3', '4', '5', '6', '7/128', '7/192', '7/256', '8']
+    else:
+        list_enc=['1', '5', '7/128', '7/192', '7/256']      #DES, Triple-DES, AES/128, AES/192 and AES/256
+    # Hash algorithms: 
+    if args.alltransforms:
+        list_hash=['1','2', '3', '4', '5', '6']
+    else:
+        list_hash=['1','2']                                 #MD5 and SHA1
+    # Authentication methods: 
+    if args.alltransforms:
+        list_auth=['1','2', '3', '4', '5', '6', '7', '8', '64221', '65001']
+    else:
+        list_auth=['1','3', '64221', '65001']               #Pre-Shared Key, RSA Signatures, Hybrid Mode and XAUTH
+    # Diffie-Hellman groups: 
+    if args.alltransforms:
+        list_dhgroup=['1','2', '3', '4', '5', '6', '7', '8', '9', '10', '11','12', '13', '14', '15', '16', '17', '18']
+    else:
+        list_dhgroup=['1','2', '5']                         #1, 2 and 5
     
     for enc in list_enc:
         for hash in list_hash:
@@ -59,6 +73,9 @@ def main(argv):
                         
                     if args.id:
                         ikescan_command = ikescan_command + " --id=" + args.id
+                        
+                    if args.idtype:
+                        ikescan_command = ikescan_command + " --idtype=" + args.idtype
                     
                     p = subprocess.Popen(ikescan_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE,shell=True)
                     p.wait()
@@ -88,7 +105,7 @@ def main(argv):
                             print "If this is true, psk-crack attempts will be unsuccessful!"
                         
                         #if not an extended scan and we got a handshake, exit program
-                        if args.extended == False and re.search(regex_handshake, out):
+                        if args.scanall == False and re.search(regex_handshake, out):
                             sys.exit()
                     
 if __name__ == "__main__":
