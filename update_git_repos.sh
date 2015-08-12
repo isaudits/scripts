06c979260759e1e79702b53feb2485c2d96a8959
Matthew C Jones <mjones@isaudits.com>
2015-08-12 16:41:54 -0400

    minor fixes

--- update_git_repos.sh
+++ update_git_repos.sh
@@ -3,15 +3,15 @@
 # script courtesy of https://gist.github.com/douglas/1287372
 
 # store the current dir
-CUR_DIR=$(pwd)
+CUR_DIR="$(pwd)"
 
 # Let the person running the script know what's going on.
-echo "\n\033[1mPulling in latest changes for all repositories...\033[0m\n"
+echo -e "\n\033[1mPulling in latest changes for all repositories...\033[0m\n"
 
 # Find all git repositories and update it to the master latest revision
 for i in $(find . -name ".git" | cut -c 3-); do
     echo "";
-    echo "\033[33m"+$i+"\033[0m";
+    echo -e "\033[33m"+$i+"\033[0m";
 
     # We have to go to the .git parent directory to call the pull command
     cd "$i";
@@ -24,4 +24,4 @@ for i in $(find . -name ".git" | cut -c 3-); do
     cd $CUR_DIR
 done
 
-echo "\n\033[32mComplete!\033[0m\n"
+echo -e "\n\033[32mComplete!\033[0m\n"
