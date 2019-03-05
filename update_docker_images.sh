4244fb9a34ea7366e38def2ba37da1dfa2a4c8d7
Matthew C. Jones, CPA, CISA, OSCP, CCFE <mjones@isaudits.com>
2019-03-05 16:23:56 -0500

    tweak docker update script to get tags other than latest

--- update_docker_images.sh
+++ update_docker_images.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Updates all docker images on filesystem and deletes old images
 
-docker images | grep -v REPOSITORY | awk '{print $1}' | xargs -L1 docker pull
+#docker images | grep -v REPOSITORY | awk '{print $1}' | xargs -L1 docker pull
+docker images --format "{{.Repository}}:{{.Tag}}" | xargs -L1 docker pull
 docker image prune -f
