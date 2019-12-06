1657271f742752e0cccaa1eda99f0ce5902fed5c
Matthew C. Jones, CPA, CISA, OSCP, CCFE <mjones@isaudits.com>
2019-12-05 19:16:57 -0500

    get more info from GPO report exports

--- powershell/export_all_GPO_reports.ps1
+++ powershell/export_all_GPO_reports.ps1
@@ -2,3 +2,5 @@
 
 Import-Module GroupPolicy 
 Get-GPO -all | % { Get-GPOReport -GUID $_.id -ReportType HTML -Path "GPO_$($_.displayName).html" }
+Get-GPOReport -All -ReportType html -Path GPO_all.html
+Get-GPOReport -All -ReportType xml -Path GPO_all.xml
