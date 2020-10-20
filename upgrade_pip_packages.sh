cc3e1d1547a4bb419964b8a89ac3acac784cb56f
Matthew C. Jones, CPA, CISA, OSCP, CCFE <mjones@isaudits.com>
2020-10-20 13:18:49 -0400

    update both pip2 and pip3

--- upgrade_pip_packages.sh
+++ upgrade_pip_packages.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
+pip2 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip2 install -U
+pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U
