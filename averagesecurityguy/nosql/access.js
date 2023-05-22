6ea5ee1425a5b3a822b9af6e90ef87f3ac828a97
Matt Jones <matt.jones@symphona.us>
2023-05-22 15:33:45 -0400

    convert to submodule

--- averagesecurityguy/nosql/access.js
+++
@@ -1,6 +0,0 @@
-
-try {
-    cursor = db.startup_log.findOne();
-    print("Hostname: " + cursor['hostname']);
-    print("Version: " + cursor['buildinfo']['version']);
-} catch(err) {}
--- averagesecurityguy/webapp/proof.js
+++
@@ -1,4 +0,0 @@
-var target = document.getElemntByTagName("body");
-var p = document.createElement("p");
-var text = p.createTextNode("XSS was here.");
-target.appendChild(text);
